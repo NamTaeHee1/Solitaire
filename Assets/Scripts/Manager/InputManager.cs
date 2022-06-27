fdda8810f51a5bbd8bb66b4424231db5e938443a
남태희 <68994412+NamTaeHee1@users.noreply.github.com>
2022-06-27 23:21:46 +0900

    [UPDATE] GameScene + InputManager
    GameScene에 InputManager 추가

--- Assets/Scripts/Manager/InputManager.cs
+++ Assets/Scripts/Manager/InputManager.cs
@@ -15,11 +15,17 @@ public class InputManager : MonoBehaviour
 
 	private void Update()
 	{
+		if (Input.GetMouseButton(0) || Input.GetMouseButtonUp(0))
+		{
+
+		}
+	}
+
+	private void InputMouseUpdate()
+	{
 		if (Input.GetMouseButton(0))
 			_InputType = InputType.DRAG;
 		if (Input.GetMouseButtonUp(0))
 			_InputType = InputType.IDLE;
-
-		print(_InputType);
 	}
 }
