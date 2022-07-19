54bc515d45fb6ddab7bd4e63b9611ed814dc056c
남태희 <68994412+NamTaeHee1@users.noreply.github.com>
2022-07-20 00:34:15 +0900

    [UPDATE] Test + TestClick
    Test Scene 수정

--- Assets/Scripts/TestClick.cs
+++ Assets/Scripts/TestClick.cs
@@ -1,18 +1,36 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
 
-public class TestClick : MonoBehaviour
+public class TestClick : MonoBehaviour, IDragHandler, IPointerDownHandler
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+	public Vector2 Pos = Vector2.zero;
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+	public void OnDrag(PointerEventData eventData)
+	{
+		GetClickPos(eventData);
+	}
+
+	public void OnPointerDown(PointerEventData eventData)
+	{
+		GetClickPos(eventData);
+	}
+
+	private void Update()
+	{
+		Vector2 UIMousePos = Vector2.zero;
+		if (RectTransformUtility.ScreenPointToLocalPointInRectangle(GameObject.Find("Canvas").GetComponent<RectTransform>(), Camera.main.WorldToScreenPoint(Input.mousePosition), Camera.main, out Vector2 pos))
+			UIMousePos = pos;
+		Debug.Log($"MousePosition : {Input.mousePosition}, Pos : {Pos}, UI.MousePosition : {UIMousePos}, " +
+			$"UI.Pos : {Camera.main.ScreenToWorldPoint(Pos)}, Image.AnchorPos : {GetComponent<RectTransform>().anchoredPosition}");
+	}
+
+	private void GetClickPos(PointerEventData eventData)
+	{
+		if (RectTransformUtility.ScreenPointToLocalPointInRectangle(GetComponent<RectTransform>(), // Card Image의 클릭한 부분을 가져오는 코드
+		eventData.position, eventData.pressEventCamera, out Vector2 localCursor))
+			Pos = localCursor;
+	}
 }
