476f5740457f43444bde8c2f2e0736de14a3e95f
남태희 <68994412+NamTaeHee1@users.noreply.github.com>
2022-07-23 23:18:52 +0900

    [UPDATE] TEST + TestClick
    클릭 기준 이동 TEST Scene에서 성공

--- Assets/Scripts/TestClick.cs
+++ Assets/Scripts/TestClick.cs
@@ -4,30 +4,13 @@ using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.EventSystems;
 
-public class TestClick : MonoBehaviour
+public class TestClick : MonoBehaviour, IDragHandler
 {
-	public RectTransform PanelUIRect;
-	private Camera MainCam;
-	public RectTransform ImageUIRect;
+	public RectTransform ImageRect;
+	public RectTransform CanvasRect;
 
-	public Vector3 ClickVec = Vector3.zero;
-
-	private void Start()
+	public void OnDrag(PointerEventData eventData)
 	{
-		MainCam = Camera.main;
-	}
-
-	private void Update()
-	{
-		if (Input.GetMouseButton(0) && RectTransformUtility.RectangleContainsScreenPoint(ImageUIRect, Input.mousePosition, MainCam))
-		{
-			RectTransformUtility.ScreenPointToLocalPointInRectangle(PanelUIRect, Input.mousePosition, MainCam, out Vector2 localPos);
-			if (ClickVec == Vector3.zero)
-				ClickVec = localPos;
-			ImageUIRect.localPosition = ImageUIRect.localPosition - new Vector3(ClickVec.x, ClickVec.y, 0);
-			//ImageUIRect.localPosition = ((Vector3)localPos - ImageUIRect.localPosition);
-		}
-		if (Input.GetMouseButtonUp(0))
-			ClickVec = Vector3.zero;
+		ImageRect.anchoredPosition += eventData.delta;
 	}
 }
