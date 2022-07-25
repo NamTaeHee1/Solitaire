470ea29933e525c3a564fd613b60cf58d89ab787
남태희 <68994412+NamTaeHee1@users.noreply.github.com>
2022-07-26 02:16:38 +0900

    [UPDATE] Card + GameScene
    Card Move 함수 구현

--- Assets/Scripts/Card/Card.cs
+++ Assets/Scripts/Card/Card.cs
@@ -21,6 +21,7 @@ public class Card : MonoBehaviour, IDragHandler, IPointerDownHandler, IPointerUp
 		this.CardName = CardName;
 	}
 
+	#region IHandler Functions
 	public void OnPointerDown(PointerEventData eventData)
 	{
 		SetCardState(State.CardState.CLICKED);
@@ -30,21 +31,47 @@ public class Card : MonoBehaviour, IDragHandler, IPointerDownHandler, IPointerUp
 	public void OnDrag(PointerEventData eventData)
 	{
 		SetCardState(State.CardState.DRAGING);
-		Drag(eventData);
 		SetCurrentInputCard(this);
-	}
-
-	private void Drag(PointerEventData eventData)
-	{
-		RectTransform CardRect = transform.GetComponent<RectTransform>();
-		CardRect.anchoredPosition = Vector2.Lerp(CardRect.anchoredPosition, CardRect.anchoredPosition + eventData.delta, 1f);
-		Debug.Log(Time.deltaTime * 1000.0f);
+		Drag(eventData);
 	}
 
 	public void OnPointerUp(PointerEventData eventData)
 	{
 		SetCardState(State.CardState.IDLE);
 		SetCurrentInputCard(null);
+		StartCoroutine(MoveCard());
+	}
+	#endregion
+
+	#region Move & Drag Function
+
+	[SerializeField] private RectTransform CardRect;
+
+	private void Drag(PointerEventData eventData)
+	{
+		CardRect.anchoredPosition = Vector2.Lerp(CardRect.anchoredPosition, CardRect.anchoredPosition + eventData.delta, 1f);
+	}
+
+	private void Move()
+	{
+		while (Vector3.Distance(CardRect.anchoredPosition, GameObject.Find("CardSpawner").transform.position) > 0.01f)
+		{
+			Debug.Log("몇번작동하는거야 도데체");
+			CardRect.anchoredPosition = Vector3.Lerp(CardRect.localPosition, GameObject.Find("CardSpawner").transform.position, 0.5f);
+		}
+	}
+	#endregion
+
+	IEnumerator MoveCard()
+	{
+		float t = 0;
+
+		while (Vector3.Distance(CardRect.anchoredPosition, GameObject.Find("CardSpawner").transform.position) > 0.01f)
+		{
+			t += Time.deltaTime;
+			CardRect.anchoredPosition = Vector3.Lerp(CardRect.localPosition, GameObject.Find("CardSpawner").transform.position, t);
+			yield return null;
+		}
 	}
 
 	private void SetCardState(State.CardState state) => CardState = state;
