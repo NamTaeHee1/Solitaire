246c9cfe05bb457bbe90a809add76ac988136fb3
남태희 <68994412+NamTaeHee1@users.noreply.github.com>
2022-07-31 22:50:19 +0900

    [UPDATE] Point
    Place -> Point 스크립트 이름 변경

--- Assets/Scripts/Point/Point.cs
+++ Assets/Scripts/Point/Point.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Place : MonoBehaviour
+public class Point : MonoBehaviour
 {
 	public PlaceType _placeType = PlaceType.S;
 }
