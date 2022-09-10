87ae438ace08f9ce6fec4c795a37d2c277fbbeb7
남태희 <68994412+NamTaeHee1@users.noreply.github.com>
2022-09-10 22:08:33 +0900

    [UPDATE] PointManager
    PointManager 각 지점 변수 생성

--- Assets/Scripts/Point/PointManager.cs
+++ Assets/Scripts/Point/PointManager.cs
@@ -11,9 +11,32 @@ public class PointManager : MonoBehaviour
 
 	private static PointManager _instance;
 
+	public Point[] K;
+	public Point[] A;
+	public Point SelectCardPoint;
+
 	private void Awake()
 	{
 		_instance = this;
 		DontDestroyOnLoad(this);
+
+		GameObject K_Points = GameObject.Find("K_Points");
+		Point[] K_Array = new Point[K_Points.transform.childCount];
+
+		for (int i = 0; i < K_Points.transform.childCount; i++)
+		{
+			K[i] = K_Points.transform.GetChild(i).GetComponent<Point>();
+		}
+
+		GameObject A_Points = GameObject.Find("A_Points");
+		Point[] A_Array = new Point[A_Points.transform.childCount];
+
+		for (int i = 0; i < A_Points.transform.childCount; i++)
+		{
+			A[i] = A_Points.transform.GetChild(i).GetComponent<Point>();
+		}
+
+		SelectCardPoint = GameObject.Find("SelectCardPoint").GetComponent<Point>();
 	}
+
 }
