4c49f33c07c4b1e7aa88b472772ddb88ff90287c
남태희 <68994412+NamTaeHee1@users.noreply.github.com>
2024-01-04 22:43:40 +0900

    [UPDATE] Card Move 함수 수정

--- Assets/Scripts/InGame/Card.cs
+++ Assets/Scripts/InGame/Card.cs
@@ -175,6 +175,7 @@ public class Card : Point, IPointerDownHandler, IBeginDragHandler, IDragHandler,
 								_pointLastCard.Show(ECardDirection.FRONT);
 						}
 
+
 						curPoint = card;
 					}
 					else if(toPoint is Point)
